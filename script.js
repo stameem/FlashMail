b659d4fb6df1ad72b0d3b5252f79dacd5653a63d
stameem <30806649+stameem@users.noreply.github.com>
2023-08-24 00:38:23 +0530

    Create script.js
    The JavaScript code for opening a web link or automating the process of sending an email with just one click.

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("option1").addEventListener("click", function() {
+window.open("mailto:example@mail.com?subject=TEST%20SUBJECT%20ADD%20DETAILS%20HERE%20FOR%20SUBJECT&body=EXAMPLE%20BODY%20FOR%20SPACE%20USE%20PERCETAGE%20SYMBOL%20AND%20TWENTY%20IN%20NUMBER%0ATO%20MOVE%20TO%20NEXT%20LINE%20USE%20PERCENTAGE%20SYMBOL%20AND%20ZERO%20IN%20NUMBER%20AND%20A%20IN%20WORDS");
+    });
+    document.getElementById("option2").addEventListener("click", function() {
+window.open("mailto:example1@mail.com?subject=TEST%20SUBJECT%20ADD%20DETAILS%20HERE%20FOR%20SUBJECT&body=EXAMPLE%20BODY%20FOR%20SPACE%20USE%20PERCETAGE%20SYMBOL%20AND%20TWENTY%20IN%20NUMBER%0ATO%20MOVE%20TO%20NEXT%20LINE%20USE%20PERCENTAGE%20SYMBOL%20AND%20ZERO%20IN%20NUMBER%20AND%20A%20IN%20WORDS");
+    });
+    
+    document.getElementById("option3").addEventListener("click", function() {
+window.open("https://www.facebook.com")
+    });
